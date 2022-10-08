c04da1200af4e578a1185a704d833c63d3cd1c3d
Andres <spartansup18@gmail.com>
2022-10-08 13:24:39 -0700

    added ability to reset ball after stopping

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ function update() {
     projection.angle -= 0.05;
   }
   else if( switching == true){
-    console.log("helo")
+    //console.log("helo")
     projection.angle += 0.05;
     //console.log(Math.round(projection.angle))
   }
@@ -73,7 +73,7 @@ function update() {
   }
   if(Math.round(projection.angle) > 0){
     //console.log(Math.round(projection.angle))
-    console.log("yo")
+    //console.log("yo")
     switching = false;
   }
 
@@ -81,9 +81,9 @@ function update() {
   line(projection.pin, vec(projection.pin).addWithAngle(projection.angle, projection.length));
 
   if(input.isPressed && charge<0.15 && shot == false){
-    charge+=.003
-    shiftspeed += charge
-    dropspeed += charge
+    charge+=.003;
+    shiftspeed += charge;
+    dropspeed += charge;
     //shot = true;
 
   }
@@ -98,7 +98,7 @@ function update() {
      shiftspeed *=  manipshift;
      manipdrop = -3 - projection.angle;
      dropspeed *=  manipdrop;
-     console.log("PROJECTION CHECK")
+     //console.log("PROJECTION CHECK")
     }
     else if(Math.round(projection.angle == -1.5)){
       shiftspeed =0; 
@@ -118,7 +118,7 @@ function update() {
   if (ball.y <=10 || Math.round(ball.y >= 90) ){
     bonks-= 0.01;
     //ball.x = 10;
-    dropspeed *= -1  ;
+    dropspeed *= -1 ;
     //dropspeed *= -1;
   }
   if (shot == true) {
@@ -128,6 +128,14 @@ function update() {
     if (dropspeed != 0){
       dropspeed = dropspeed/1.005;
     }
+
+    if((Math.abs(shiftspeed)<.005) && (Math.abs(dropspeed)<.005)){
+      console.log("zero");
+      shiftspeed = 0;
+      dropspeed = 0;
+      charge = 0;
+      shot = false;
+    }
     ball.y += dropspeed;
     ball.x += shiftspeed;
     box(ball, 3);
@@ -136,7 +144,10 @@ function update() {
   }
 
   if (shot == false){
-    box(ball, 3);
+    ball.x =G.WIDTH/2 
+    ball.y = 4 * G.HEIGHT/5
+    projection.length = 7;
+    //box(ball, 3);
   }
 
   pins.forEach((s) => {
